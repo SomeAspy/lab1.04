f7962a572c056b494c765d7419619e4534c31d42
Aiden <33640860+SomeAspy@users.noreply.github.com>
2021-02-23 09:13:23 -0500

    newline

--- magic_genie.py
+++ magic_genie.py
@@ -7,13 +7,13 @@
 part=input("run part 1 or 2?")
 
 print("I am a genie. You have three wishes.")
-w1=input("What would you like to wish for?")
-w2=input("What would you like to wish for?")
-w3=input("What would you like to wish for?")
+w1=input("What would you like to wish for?\n")
+w2=input("What would you like to wish for?\n")
+w3=input("What would you like to wish for?\n")
 
 if part=="1":
     print("Your wishes are "+w1+", "+w2+", and "+w3);
 elif part=="2":
     print("Your wishes are "+w2+", "+w3+", and "+w1);
 else:
-    print("You were given 2 options, and you chose neither. Great job.");
+    print("\nYou were given 2 options, and you chose neither. Great job.");
